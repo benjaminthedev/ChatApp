a09cb9629533acc582a529241be4891c9dd78f29
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-10-14 19:29:20 +0100

    Adding action between seques

---
+++ ChatApp/ChatApp/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  ChatApp
+//
+//  Created by Benjamin on 14/10/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //outlets
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+    }
+
+
+
+}
