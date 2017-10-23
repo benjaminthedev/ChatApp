37ccab697f171714e9ee2711838eda2e7c14ab04
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-10-23 13:19:22 +0100

    updated login screens

---
+++ ChatApp/ChatApp/Util/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Benjamin on 21/10/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
