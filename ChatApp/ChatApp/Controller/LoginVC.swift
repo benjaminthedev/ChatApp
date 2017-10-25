399a0a898ea3c1b91a30a3f17e7084075bed7e11
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-10-25 14:13:39 +0100

    Added log in screens

---
+++ ChatApp/ChatApp/Controller/LoginVC.swift
@@ -0,0 +1,27 @@
+//
+//  LoginVC.swift
+//  ChatApp
+//
+//  Created by Benjamin on 25/10/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+   
+
+}
