9d0425950714e43661238c60f0c64688c50d5352
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-10-27 10:32:36 +0100

    Added Alamofire HTTP requests etc

--- ChatApp/ChatApp/Util/Constants.swift
+++ ChatApp/ChatApp/Util/Constants.swift
@@ -8,8 +8,24 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL CONSTANTS
+let BASE_URL = "https://chattydb.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+
+//User Defaults Baby!
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
