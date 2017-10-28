04e9a5cdaa041ebeafa6d08150606d1cae1b1d31
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-10-28 07:25:12 +0100

    users can log in now

--- ChatApp/ChatApp/Util/Constants.swift
+++ ChatApp/ChatApp/Util/Constants.swift
@@ -11,8 +11,10 @@ import Foundation
 typealias CompletionHandler = (_ Success: Bool) -> ()
 
 //URL CONSTANTS
+
 let BASE_URL = "https://chattydb.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 //Segues
 
@@ -20,12 +22,14 @@ let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
 
-
-
 //User Defaults Baby!
 
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
+//Header
+let HEADER = [
+    "Content-Type" : "application/json; charset=uft-8"
+]
 
