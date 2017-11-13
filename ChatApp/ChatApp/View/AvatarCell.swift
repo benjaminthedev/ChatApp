61674ce8e1b6bbd99a5f1c0c2ffa5b6074e93bc5
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-11-13 16:05:10 +0000

    updated VC Cells

---
+++ ChatApp/ChatApp/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  ChatApp
+//
+//  Created by Benjamin on 13/11/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    
+    func setUpView(){
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
