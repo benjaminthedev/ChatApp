8823a6e90f5b23f474bc61d573c2086fd25f81f8
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2017-11-13 15:08:03 +0000

    Add avatar picker

---
+++ ChatApp/ChatApp/Controller/AvatarPickerVC.swift
@@ -0,0 +1,49 @@
+//
+//  AvatarPickerVC.swift
+//  ChatApp
+//
+//  Created by Benjamin on 13/11/2017.
+//  Copyright © 2017 Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    //Me ol outles..
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+       
+    }
+
+
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+        
+    }
+    
+}
